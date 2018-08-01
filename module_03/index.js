a8372aea3e1bda635162fe2e70d8ff6e227355bb
aleal <adriano.leal@exlcusivkey.pt>
2018-08-01 17:24:44 +0100

    Model de usuário e autoload

--- module_03/index.js
+++ module_03/index.js
@@ -1,7 +1,23 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create(
+  {
+    name: 'Adriano',
+    username: 'adbrum',
+    email: 'adbrum@outlook.com',
+    password: '123',
+  },
+  () => {
+    console.log('OK');
+  },
+);
 
 app.listen(3000);
