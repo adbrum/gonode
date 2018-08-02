61cfc9f04d46a981e9ca3af5651edb2fdcd5bdc7
aleal <adriano.leal@exlcusivkey.pt>
2018-08-02 19:04:43 +0100

    Middleware de autenticação

--- module_03/app/routes.js
+++ module_03/app/routes.js
@@ -3,6 +3,8 @@ const requireDir = require('require-dir');
 
 const routes = express.Router();
 
+const authMiddlewares = require('./middlewares/auth');
+
 const controllers = requireDir('./controllers');
 
 /**
@@ -10,5 +12,16 @@ const controllers = requireDir('./controllers');
  */
 routes.post('/signup', controllers.authController.signup);
 routes.post('/signin', controllers.authController.signin);
+
+/**
+ *Auth routes
+ */
+routes.use(authMiddlewares);
+
+routes.get('/tweets', (req, res) => {
+  console.log(req.userId);
+
+  res.send('OK');
+});
 
 module.exports = routes;
